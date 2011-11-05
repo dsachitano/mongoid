370a6bf57238585d552460fae3bebd00900db1f6
David Sachitano <dsachita@gmail.com>
2011-11-05 02:03:05 -0700

    changed spec to look more like spec guidelines

--- spec/functional/mongoid/identity_conversions_spec.rb
+++ spec/functional/mongoid/identity_conversions_spec.rb
@@ -1,74 +1,78 @@
 require "spec_helper"
 
-describe Mongoid::Identity do
+describe Mongoid::Extensions::ObjectId::Conversions do
 
   before do
     Motherboard.delete_all
     Ram.delete_all
   end
 
-  context "when identity type is a String" do
+  describe ".convert" do
 
-    context "and setting id as a String" do
-      before do
-        Motherboard.delete_all
-        Ram.delete_all
-        Motherboard.identity(:type => String)
-        Motherboard.create!(:id => 1)
-        Ram.create!(:motherboard_id => "1")
+    context "when identity type is a String" do
+
+      context "and setting id as a String" do
+        before do
+          Motherboard.delete_all
+          Ram.delete_all
+          Motherboard.identity(:type => String)
+          Motherboard.create!(:id => 1)
+          Ram.create!(:motherboard_id => "1")
+        end
+
+        #let(:person) do
+        #  Person.find.first
+        #end
+
+        it "it is still a string" do
+          Ram.first.motherboard.id.should == "1"
+        end
       end
 
-      it "it is still a string" do
-        Ram.first.motherboard.id.should == "1"
+      context "and setting id as an Integer" do
+        before do
+          Motherboard.delete_all
+          Ram.delete_all
+          Motherboard.identity(:type => String)
+          Motherboard.create!(:id => 1)
+          Ram.create!(:motherboard_id => 1)
+        end
+
+        it "it is still a string" do
+          Ram.first.motherboard.id.should == "1"
+        end
       end
     end
 
-    context "and setting id as an Integer" do
-      before do
-        Motherboard.delete_all
-        Ram.delete_all
-        Motherboard.identity(:type => String)
-        Motherboard.create!(:id => 1)
-        Ram.create!(:motherboard_id => 1)
+    context "when identity type is an Integer" do
+
+      context "and setting id as a String" do
+        before do
+          Motherboard.delete_all
+          Ram.delete_all
+          Motherboard.identity(:type => Integer)
+          Motherboard.create!(:id => 1)
+          Ram.create!(:motherboard_id => "1")
+        end
+
+        it "it is still an integer" do
+          Ram.first.motherboard.id.should == 1
+        end
       end
 
-      it "it is still a string" do
-        Ram.first.motherboard.id.should == "1"
+      context "and setting id as an Integer" do
+        before do
+          Motherboard.delete_all
+          Ram.delete_all
+          Motherboard.identity(:type => Integer)
+          Motherboard.create!(:id => 1)
+          Ram.create!(:motherboard_id => 1)
+        end
+
+        it "it is still an integer" do
+          Ram.first.motherboard.id.should == 1
+        end
       end
     end
-
   end
-
-  context "when identity type is an Integer" do
-
-    context "and setting id as a String" do
-      before do
-        Motherboard.delete_all
-        Ram.delete_all
-        Motherboard.identity(:type => Integer)
-        Motherboard.create!(:id => 1)
-        Ram.create!(:motherboard_id => "1")
-      end
-
-      it "it is still an integer" do
-        Ram.first.motherboard.id.should == 1
-      end
-    end
-
-    context "and setting id as an Integer" do
-      before do
-        Motherboard.delete_all
-        Ram.delete_all
-        Motherboard.identity(:type => Integer)
-        Motherboard.create!(:id => 1)
-        Ram.create!(:motherboard_id => 1)
-      end
-
-      it "gives back an integer" do
-        Ram.first.motherboard.id.should == 1
-      end
-    end
-
-  end
-
 end
