925d21f0e75fc367c722a0266114e6f6151a19b6
David Sachitano <dsachita@gmail.com>
2011-11-04 20:20:17 -0700

    adding back Integer to String case

--- lib/mongoid/extensions/object_id/conversions.rb
+++ lib/mongoid/extensions/object_id/conversions.rb
@@ -35,9 +35,9 @@ module Mongoid #:nodoc:
 
           if !klass.using_object_ids?
             tmp_type = klass.fields["_id"].options[:type]
-            #if tmp_type.eql?(String)
-            #  return args.to_s
-            #end
+            if tmp_type.eql?(::String)
+              return "#{args}" if args.is_a?(::Integer)
+            end
             if tmp_type.eql?(::Integer)
                 return Integer(args) if args.is_a?(::String)
             end
